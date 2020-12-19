9bff29f010718944747371a5765f3f730bb03904
Ogulcan Turk <hi@ogulcan.dev>
2020-12-19 18:34:58 +0300

    first commit

---
+++ readme.md
@@ -0,0 +1,18 @@
+# Parcel Vue Example
+
+A vue project packaged with Parcel. An easy example to get started with Vue!
+
+## Start
+
+```bash
+yarn start
+```
+
+## Build
+
+```bash
+yarn build
+```
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ src/app.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="app">
+        <h1>{{title}}</h1>
+        <p>{{message}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+    data () {
+        return {
+            title: 'Parcel Vue Example',
+            message: 'Hello!'
+        }
+    }
+}
+</script>
